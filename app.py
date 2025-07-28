6e8c9e2f0b88020fe734bc9f32913e4d05aa7bb7
Vineeth Reddy <saivineeth4382@gmail.com>
2025-07-28 23:02:08 +0530

    Switched to smaller model for Streamlit compatibility

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def is_offensive(text):
 # Load Blenderbot model + tokenizer
 @st.cache_resource
 def load_model():
-    model_name = "facebook/blenderbot-1B-distill"
+    model_name = "facebook/blenderbot-400M-distill"
     tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
     model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
     return tokenizer, model
